df0f065134de925894ca14e0b246b7357d8ec730
Charlotte Davies <charlottemdavies@hotmail.co.uk>
2019-08-21 16:36:17 +0100

    added outfit options

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # weather-to-node
+
+Run npm init to start a project - $ npm init 
+
+Run node (and the name of your file) to run the file - $ node index
+
+To install other dependencies run npm install (name of dependency) --save - $ npm install axios --save
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const axios = require("axios");
 
+const outerWear = [
+  { name: "Cashmere coat", maxTemp: 15, minTemp: 0, waterRating: 0},
+  { name: "Rain mac", maxTemp: 25, minTemp: 10, waterRating: 1},
+  { name: "Duffel coat", maxTemp: 12, minTemp: -5, waterRating: 0.5}
+];
+
 async function getWeather() {
   try {
     const result = await axios.get(
@@ -7,12 +13,33 @@ async function getWeather() {
     );
 
     const currentWeather = result.data.currently;
-    const { temperature } = currentWeather;
+    const { temperature, precipProbability } = currentWeather;
+
+    const outfit = getOutfit(temperature, precipProbability);
+
+    console.log(precipProbability);
 
     console.log(`Current temperature: ${temperature}`);
+    console.log(`Today's a great day for a ${outfit}.`);
+
+
   } catch (e) {
     console.error(e);
   }
+}
+
+function getOutfit(temperature, chanceOfRain) { // chanceOfRain is precipProbability (the second argument)
+
+  const options = outerWear.filter(outfit =>
+    temperature >= outfit.minTemp &&
+    temperature <= outfit.maxTemp &&
+    chanceOfRain <= outfit.waterRating
+  );
+
+  const randomIndex = Math.floor(Math.random() * options.length);
+
+  return options[randomIndex].name;
+
 }
 
 getWeather();
