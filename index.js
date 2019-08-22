0309ccd0b2b4b0647fe226a716990ddf183f258f
Charlotte Davies <charlottemdavies@hotmail.co.uk>
2019-08-22 09:05:21 +0100

    refactored code

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const bottoms = [
 ];
 
 const tops = [
-  { name: "merino jumper", maxTemp: 15, minTemp: 3},
-  { name: "silky strap top", maxTemp: 25, minTemp: 5},
-  { name: "plain t shirt", maxTemp: 100, minTemp: 10},
-  { name: "cardigan", maxTemp: 28, minTemp: 0},
-  { name: "wooly jumper", maxTemp: 8, minTemp: -100},
-  { name: "shirt", maxTemp: 20, minTemp: -100}
+  { name: "merino jumper", maxTemp: 15, minTemp: 3, waterRating: 0},
+  { name: "silky strap top", maxTemp: 25, minTemp: 5, waterRating: 0},
+  { name: "plain t shirt", maxTemp: 100, minTemp: 10, waterRating: 0},
+  { name: "cardigan", maxTemp: 28, minTemp: 0, waterRating: 0},
+  { name: "wooly jumper", maxTemp: 8, minTemp: -100, waterRating: 0},
+  { name: "shirt", maxTemp: 20, minTemp: -100, waterRating: 0}
 ];
 
 const shoes = [
@@ -32,8 +32,6 @@ const shoes = [
   { name: "flats", maxTemp: 100, minTemp: 8, waterRating: 0.5}
 ];
 
-
-
 async function getWeather() {
   try {
     const result = await axios.get(
@@ -43,12 +41,10 @@ async function getWeather() {
     const currentWeather = result.data.currently;
     const { temperature, precipProbability } = currentWeather;
 
-    const coat = getOuterWear(temperature, precipProbability);
-    const todaysBottom = getBottoms(temperature);
-    const todaysTop = getTops(temperature);
-    const todaysShoes = getShoes(temperature, precipProbability);
-
-    // console.log(precipProbability);
+    const coat = getWardrobe(temperature, precipProbability, outerWear);
+    const todaysBottom = getWardrobe(temperature, precipProbability, bottoms);
+    const todaysTop = getWardrobe(temperature, precipProbability, tops);
+    const todaysShoes = getWardrobe(temperature, precipProbability, shoes);
 
     console.log(`Current temperature: ${temperature}`);
     console.log(`Today's a great day for a ${coat}.`);
@@ -61,46 +57,9 @@ async function getWeather() {
 
 getWeather();
 
-function getOuterWear(temperature, chanceOfRain) { // chanceOfRain is precipProbability (the second argument)
+function getWardrobe(temperature, chanceOfRain, wardrobe) { // chanceOfRain is precipProbability (the second argument)
 
-  const options = outerWear.filter(outfit =>
-    temperature >= outfit.minTemp &&
-    temperature <= outfit.maxTemp &&
-    chanceOfRain <= outfit.waterRating
-  );
-
-  const randomIndex = Math.floor(Math.random() * options.length);
-
-  return options[randomIndex].name;
-
-};
-
-function getBottoms(temperature) {
-  const options = bottoms.filter(outfit =>
-    temperature >= outfit.minTemp &&
-    temperature <= outfit.maxTemp
-  );
-
-  const randomIndex = Math.floor(Math.random() * options.length);
-
-  return options[randomIndex].name;
-};
-
-
-function getTops(temperature) {
-  const options = tops.filter(outfit =>
-    temperature >= outfit.minTemp &&
-    temperature <= outfit.maxTemp
-  );
-
-  const randomIndex = Math.floor(Math.random() * options.length);
-
-  return options[randomIndex].name;
-};
-
-function getShoes(temperature, chanceOfRain) {
-
-  const options = shoes.filter(outfit =>
+  const options = wardrobe.filter(outfit =>
     temperature >= outfit.minTemp &&
     temperature <= outfit.maxTemp &&
     chanceOfRain <= outfit.waterRating
