34577e77a93dd25fff0795ab4e859534d1604893
Charlotte Davies <charlottemdavies@hotmail.co.uk>
2019-08-22 08:52:18 +0100

    added more wardrobe items

--- index.js
+++ index.js
@@ -6,6 +6,34 @@ const outerWear = [
   { name: "Duffel coat", maxTemp: 12, minTemp: -5, waterRating: 0.5}
 ];
 
+const bottoms = [
+  { name: "black jeans", maxTemp: 15, minTemp: -100, waterRating: 1},
+  { name: "blue jeans", maxTemp: 15, minTemp: -100, waterRating: 1},
+  { name: "white jeans", maxTemp: 15, minTemp: -100, waterRating: 0},
+  { name: "floaty skirt", maxTemp: 100, minTemp: 15, waterRating: 0.5},
+  { name: "summer dress", maxTemp: 100, minTemp: 15, waterRating: 0.5},
+  { name: "sleeved dress", maxTemp: 15, minTemp: 5, waterRating: 1},
+];
+
+const tops = [
+  { name: "merino jumper", maxTemp: 15, minTemp: 3},
+  { name: "silky strap top", maxTemp: 25, minTemp: 5},
+  { name: "plain t shirt", maxTemp: 100, minTemp: 10},
+  { name: "cardigan", maxTemp: 28, minTemp: 0},
+  { name: "wooly jumper", maxTemp: 8, minTemp: -100},
+  { name: "shirt", maxTemp: 20, minTemp: -100}
+];
+
+const shoes = [
+  { name: "plimsols", maxTemp: 100, minTemp: 10, waterRating: 0},
+  { name: "trainers", maxTemp: 15, minTemp: -100, waterRating: 1},
+  { name: "high heels", maxTemp: 100, minTemp: 7, waterRating: 0.5},
+  { name: "wedges", maxTemp: 100, minTemp: 12, waterRating: 0},
+  { name: "flats", maxTemp: 100, minTemp: 8, waterRating: 0.5}
+];
+
+
+
 async function getWeather() {
   try {
     const result = await axios.get(
@@ -15,20 +43,25 @@ async function getWeather() {
     const currentWeather = result.data.currently;
     const { temperature, precipProbability } = currentWeather;
 
-    const outfit = getOutfit(temperature, precipProbability);
+    const coat = getOuterWear(temperature, precipProbability);
+    const todaysBottom = getBottoms(temperature);
+    const todaysTop = getTops(temperature);
+    const todaysShoes = getShoes(temperature, precipProbability);
 
-    console.log(precipProbability);
+    // console.log(precipProbability);
 
     console.log(`Current temperature: ${temperature}`);
-    console.log(`Today's a great day for a ${outfit}.`);
-
+    console.log(`Today's a great day for a ${coat}.`);
+    console.log(`Today you should wear ${todaysTop} with ${todaysBottom} and ${todaysShoes}.`);
 
   } catch (e) {
     console.error(e);
   }
 }
 
-function getOutfit(temperature, chanceOfRain) { // chanceOfRain is precipProbability (the second argument)
+getWeather();
+
+function getOuterWear(temperature, chanceOfRain) { // chanceOfRain is precipProbability (the second argument)
 
   const options = outerWear.filter(outfit =>
     temperature >= outfit.minTemp &&
@@ -40,6 +73,41 @@ function getOutfit(temperature, chanceOfRain) { // chanceOfRain is precipProbabi
 
   return options[randomIndex].name;
 
-}
+};
 
-getWeather();
+function getBottoms(temperature) {
+  const options = bottoms.filter(outfit =>
+    temperature >= outfit.minTemp &&
+    temperature <= outfit.maxTemp
+  );
+
+  const randomIndex = Math.floor(Math.random() * options.length);
+
+  return options[randomIndex].name;
+};
+
+
+function getTops(temperature) {
+  const options = tops.filter(outfit =>
+    temperature >= outfit.minTemp &&
+    temperature <= outfit.maxTemp
+  );
+
+  const randomIndex = Math.floor(Math.random() * options.length);
+
+  return options[randomIndex].name;
+};
+
+function getShoes(temperature, chanceOfRain) {
+
+  const options = shoes.filter(outfit =>
+    temperature >= outfit.minTemp &&
+    temperature <= outfit.maxTemp &&
+    chanceOfRain <= outfit.waterRating
+  );
+
+  const randomIndex = Math.floor(Math.random() * options.length);
+
+  return options[randomIndex].name;
+
+};
